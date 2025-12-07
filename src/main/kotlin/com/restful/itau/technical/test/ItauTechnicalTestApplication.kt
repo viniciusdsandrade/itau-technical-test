d0ff2ccbbb3c1a4c3ed25cb24484e843e7c82a8d
andrade <viniciusdsandrade0662@gmail.com>
2025-12-07 03:46:27 -0300

    setup:

---
+++ src/main/kotlin/com/restful/itau/technical/test/ItauTechnicalTestApplication.kt
@@ -0,0 +1,11 @@
+package com.restful.itau.technical.test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItauTechnicalTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<ItauTechnicalTestApplication>(*args)
+}
